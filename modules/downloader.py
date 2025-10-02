682dd4bbecc30894f5f9646db3f3a47aaa3f7fd0
Avi1874772 <avinash.1874772@studenti.uniroma1.it>
2025-10-02 15:04:59 +0200

    folder creation

--- modules/downloader.py
+++ modules/downloader.py
@@ -10,23 +10,22 @@ class FTPdownloader:
         self.config = configparser.ConfigParser()
         self.config.read(config_file)
         
-        # FTP connection detials
-        self.host = self.config.get("FTP", "host")
-        self.user = self.config.get("FTP", "user")
-        self.password = self.config.get("FTP", "password")
-        self.port = self.config.get("FTP", "port")
-        self.remote_path = self.config.get("FTP", "remote_path")
+        self.winscp_exe_path    = self.config.get('FTP', 'winscp_exe_path').strip('"')
+        self.winscp_script_path = self.config.get('FTP', 'winscp_script_path').strip('"')
+        self.host_key           = self.config.get('FTP', 'host_key').strip('"')
+        self.ftp_host           = self.config.get('FTP', 'ftp_host').strip('"')
+        self.ftp_username       = self.config.get('FTP', 'ftp_username').strip('"')
+        self.ftp_password       = self.config.get('FTP', 'ftp_password').strip('"')
         
-        # Local directory
-        self.local_path = self.config.get("file paths", "download_path")
-        os.makedirs(self.local_path, exist_ok=True)
+        # Remote directories
+        self.remote_directory_fol       = self.config.get('FTP', 'remote_directory_fol').strip('"')
+        self.remote_directory_indicator = self.config.get('FTP', 'remote_directory_indicator').strip('"')
         
-        # yesterday date 
-        yesterday = datetime.now() - timedelta(days=1)
-        self.yesterday_str = yesterday.strftime("%Y%m%d")
-        
-        print(f"[Downloader] Config loaded. Download folder: {self.local_path}")
-        print(f"[Downloader] Yesterday's date: {self.yesterday_str}")
+        # Create daily folder 
+        today_str = datetime.now().strftime("%Y%m%d")
+        self.local_directory = os.path.join("data", "FOL", today_str)
+        os.makedirs(self.local_directory, exist_ok=True)
+        print(f"[Downloader] Created local directory: {self.local_directory}")
         
     def download_files(self):
         print(f"[Downloader] connecting to FTP: {self.host} ...")
