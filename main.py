b052f83f54ed1424b06feaf6bed8ad57564b9def
Avi1874772 <avinash.1874772@studenti.uniroma1.it>
2025-10-21 11:39:21 +0200

    changes in main file

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-import sys
-from modules.downloader import Foldownloader
+from modules.downloader import FOLDownloader
 
 def main():
-    print("========== CSA Cumulato Pipeline ==========")
-    try:
-        # Initialize downloader
-        downloader = Foldownloader(config_file="config.cfg")
-
-        # Downloading FOL zip + md5 files
-        downloader.download_files()
-
-        # Downloading Indicator files
-        downloader.download_indicator_files()
-
-        print("✅ All downloads completed successfully.")
-        print(f"Files saved in: {downloader.local_directory}")
-
-    except Exception as e:
-        print(f"❌ Pipeline failed with error: {e}")
-        sys.exit(1)
-
-if __name__ == "__main__":
+    downloader = FOLDownloader(config_file="config.cfg")
+    
+    downloader.download_files()
+    
+    downloader.download_indicator_files()
+    
+if __name__ == "__main__":    
     main()
